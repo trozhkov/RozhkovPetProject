b8daf6ab87c60b04d1c2a799b7c551957fbb10ba
anatolyRozhkov <anatolybridge@gmail.com>
2022-05-04 11:51:12 +0300

    Changed url

--- tests/settings.py
+++ tests/settings.py
@@ -9,6 +9,7 @@ JUNIT_DIR = env.str("JUNIT_DIR")
 """
 URLS
 """
-SELENIUM_URL = "http://172.25.0.2:4444"
+#SELENIUM_URL = "http://172.25.0.2:4444"
+SELENIUM_URL = "http://172.19.0.2:4444"
 PROD_URL = "http://rozhkovqa.tilda.ws/test_form"
 DEV_URL = "http://rozhkovqa.tilda.ws/test_form"
